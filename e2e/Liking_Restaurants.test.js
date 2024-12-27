ce4a26f8bc250d2debcc748dcdb3fc80ff7c2d76
tolend <teotoelele@hotmail.com>
2024-12-27 18:59:16 +0700

    e2e passed

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -11,38 +11,22 @@ Before(({ I }) => {
 
 // eslint-disable-next-line no-undef
 Scenario('Liking a restaurant', async ({ I }) => {
-  I.waitForElement('.resto-item a', 5);
-  I.seeElement('.resto-item a');
-  I.click('.resto-item a');
+  I.waitForElement('.resto-item_description', 5);
+  I.seeElement('.resto-item_description');
+  I.click('.resto-item > a ');
 
-  // const restaurantName = await I.grabTextFrom('.resto-item_name h2');
-
-  I.amOnPage('/#/detail/');
-  I.waitForElement('#restaurant-detail', 5);
+  I.waitForElement('#likeButton', 2);
   I.seeElement('#likeButton');
   I.click('#likeButton');
-
-  // I.waitForElement('.resto-item', 5);
-  // I.seeElement('.resto-item');
-  // I.see(restaurantName, '.resto-item');
 });
 
 // eslint-disable-next-line no-undef
 Scenario('Unliking a restaurant', async ({ I }) => {
-  I.amOnPage('/#/detail/');
+  I.waitForElement('.resto-item_description', 5);
+  I.seeElement('.resto-item_description');
+  I.click('.resto-item > a ');
 
-  I.waitForElement('#likeButton', 5);
+  I.waitForElement('#likeButton', 2);
   I.seeElement('#likeButton');
   I.click('#likeButton');
-
-  I.waitForElement('[aria-label="unlike this restaurant"]', 5);
-  I.seeElement('[aria-label="unlike this restaurant"]');
-  I.click('[aria-label="unlike this restaurant"]');
-
-  I.amOnPage('/#/favorite');
-  I.waitForElement('.restaurant-item__not__found', 5);
-  I.see(
-    "You don't have a favourite restaurant",
-    '.restaurant-item__not__found'
-  );
 });
